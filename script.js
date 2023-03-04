17b9b7febe90f2713c6c73ab2bbcc3d6fcef78eb
iBaDev <93536549+Ibinou@users.noreply.github.com>
2023-03-04 18:50:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-var iDeviceTypes = [  "iPad Simulator",  "iPhone Simulator",  "iPod Simulator",  "iPad",  "iPhone",  "iPod"];
+// Récupérer le user agent
+var userAgent = navigator.userAgent;
 
-if (!!navigator.platform) {
-  while (iDeviceTypes.length) {
-    if (navigator.platform === iDeviceTypes.pop()){ 
-      // Update the HTML element with the detected iDevice type
-      document.getElementById("idevice-type").textContent = navigator.platform;
-      break;
-    }
-  }
+// Vérifier si l'agent utilisateur contient "iPad"
+if (userAgent.indexOf("iPad") > -1) {
+  // Si c'est un iPad, afficher l'image de l'iPad
+  document.write("<img src='chemin/vers/image-ipad.png'>");
+} 
+// Vérifier si l'agent utilisateur contient "iPhone"
+else if (userAgent.indexOf("iPhone") > -1) {
+  // Si c'est un iPhone, afficher l'image de l'iPhone
+  document.write("<img src='chemin/vers/image-iphone.png'>");
 }
